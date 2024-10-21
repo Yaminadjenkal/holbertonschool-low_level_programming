4eb835134f69ddc83653ff7301cc63b25d74b48b
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-21 12:48:49 +0200

    hhh

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,1 +1,12 @@
-main() { // puts "Programming is like building a multilingual puzzle" return 0;//}
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: return 0 if success
+*/
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
