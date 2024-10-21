268b77ec4503a31275ff0167b585cbf57b1ad135
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-21 14:45:20 +0200

    hhh

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,4 +12,9 @@ printf("Size of char: %zu bytes\n", sizeof(char));
 printf("Size of float: %zu bytes\n", sizeof(float));
 printf("Size of double: %zu bytes\n", sizeof(double));
 return (0);
+Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)
 }
