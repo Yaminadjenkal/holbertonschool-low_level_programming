4c10b64e3b83b5c4555cdf354bb4bab78f2d83ee
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-21 14:49:22 +0200

    hhh

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of char: %zu bytes\n", sizeof(char));
-printf("Size of float: %zu bytes\n", sizeof(float));
-printf("Size of double: %zu bytes\n", sizeof(double));
+ sizeof(int));
+ sizeof(char));
+ sizeof(float));
+ sizeof(double));
 return (0);
 }
