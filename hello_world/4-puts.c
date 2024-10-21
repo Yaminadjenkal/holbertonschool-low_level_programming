201e99ed9ae425ad01315ff01d1d2123455771e0
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-21 12:15:46 +0200

    hhh

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc 4-puts.c main() puts "Programming is like building a multilingual puzzle" return 0;
+gcc 4-puts.c -c  main() puts "Programming is like building a multilingual puzzle" return 0;
