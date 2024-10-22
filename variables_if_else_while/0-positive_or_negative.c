1a13118145384fff67dc2488ca8b334aca576f3a
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-22 11:05:40 +0200

    hhh

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main ()
 {
@@ -8,5 +8,6 @@ printf("is positive");
 else if (n==0)
 printf("is zero");
 else (n>0)
-printf("is negative")
+printf("is negative");
 return 0;
+}
