45ec3221e0f4bdbad570f01ce03a1ad52dfd2da5
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-23 18:45:25 +0000

    test

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: return 0 if succes
+ */
+
+int main(void)
+{
+	for (int i = 48; i <= 57; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
