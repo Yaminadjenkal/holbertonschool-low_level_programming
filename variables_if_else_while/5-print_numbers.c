c061675563d8001e8477a291b3bc5e0b245fa17b
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-23 15:13:11 +0000

    hhh

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * Return: return 0 if succes
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		putchar (i);
+		i++;
+	}
+	return (0);
+}
