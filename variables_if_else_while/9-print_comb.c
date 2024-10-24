cee661832ab0d2ba281ef33f290ff5c6a9ab5bb5
Yaminadjenkal <mina20101951hotmail.com>
2024-10-24 12:09:10 +0000

    test

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: return 0 if success
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+	putchar (i + '0');
+	i++;
+	}
+	putchar(',');
+	putchar(' ');
+	putchar ('\n');
+return (0);
+}
