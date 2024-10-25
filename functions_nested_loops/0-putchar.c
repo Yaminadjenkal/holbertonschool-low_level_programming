539160016aeb4c0f481fe84de02f3b7b10489e65
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-25 11:53:53 +0000

    last proj

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,13 +5,14 @@
 */
 
 int main(void)
-putchar(_);
-putchar(p);
-putchar(u);
-putchar(t);
-putchar(c);
-putchar(h);
-putchar(a);
-putchar(r);
-return (0);
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+return ('0');
 }
