c55daf0e9a41bf9a4a59230d8a480734175ada7e
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-25 15:26:59 +0000

    test

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  - check the code.
+ *  - main - entry point
+ *  - returns 1 if c is lowercase
+ *  - returns 0 if c is lowercase
+ *  - returns 0 if success
+ */
+int main(void)
+{
+	char c;
+	{
+	if (c < '96' && c > '122')
+	return 1
+    } else {
+        return 0;
+    }
+}	putchar ('\n')
+	
+		
