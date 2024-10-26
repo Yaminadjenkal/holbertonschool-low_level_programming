210742876efab43c3344f6c822fe5289aeb32127
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-26 10:10:28 +0000

    test

--- functions_nested_loops/3-main.c
+++ functions_nested_loops/3-main.c
@@ -2,20 +2,19 @@
 
 /**
  * main - check the code.
- * test_islower
+ *
  * Return: Always 0.
  */
-int _islower(int c);
-
 int main(void)
 {
-int r;
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
