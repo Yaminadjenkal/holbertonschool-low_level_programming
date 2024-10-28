6de08e4dc7f3384900026a552ef5131c96418559
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-28 12:53:09 +0000

    test

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_most_numbers - print all nembers except 
  *
  * 
  */
 void print_most_numbers(void)
+{
 	int i ;
+
 	i = 0;
 	while (i <= '9')
 {
 	if (i != '2' && i != '4')
 		putchar i;
 	i++;
+}
 }
 putchar ('\n');
