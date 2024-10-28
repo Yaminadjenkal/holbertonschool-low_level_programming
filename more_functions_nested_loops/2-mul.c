329d727493109718f62005fdc933b0139500a094
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-28 09:39:48 +0000

    test

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*int mul - multiplies two integers
+*@a,b: character to be verified
+*Return: return 0 or 1
+*/
+int mul(int a, int b)
+{
+	a = 0;
+	b = 9;
+	while (a * b)
+	a++;
+	printf a;
+	return (0);
+}
