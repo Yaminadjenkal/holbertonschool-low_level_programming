dfe99cdff2aa9da54cc5a0dd7ccb3bbc59df849a
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-28 09:47:54 +0000

    test

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,14 +2,8 @@
 /**
 *int mul - multiplies two integers
 *@a,b: character to be verified
-*Return: return 0 or 1
+*Return: return a and b
 */
 int mul(int a, int b)
 {
-	a = 0;
-	b = 9;
-	while (a * b)
-	a++;
-	_putchar a;
-	return (0);
-}
+return (a * b);
