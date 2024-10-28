7abc742ea4725d7fa6b626f50a42d4f03a441071
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-28 15:08:15 +0000

    test

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,20 +5,16 @@
  */
 void more_numbers(void)
 {
-	int i, p;
-
+	int i, a;
 	i = 0;
-	p = 0;
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j <= 14; j++)
-{
-if
+	a = 0;
+	while (a < 10)
 	{
-_putchar(j / 10 + '0');
-}
-_putchar(j % 10 + '0');
-}
-_putchar('\n');
-}
+		while (a <= 14)
+			_putchar(i);
+			i++;
+	}
+	i = '0';
+	a++;
+	_putchar ('\n');
 }
