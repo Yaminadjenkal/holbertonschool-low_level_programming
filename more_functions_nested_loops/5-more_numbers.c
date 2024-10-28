4d57bea8775881674070ff5a47de5b9a7c918292
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-28 20:31:26 +0000

    test

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,26 @@
 #include "main.h"
+
 /**
- *more_numbers - print more numbers
- *
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: Can only use _putchar 3 times
  */
 void more_numbers(void)
 {
-int i , s;
-	for (i = 0; i <= 14; i++)
-		_putchar(i /10 +'0');
-		putchar (i % 10 +'0');
-	for (s = 0; s <= 10; s++)
-		_putchar(s/10 +'0');
-		_putchar(s % 10+'0');
+	int c, i;
+
+	c = 0;
+	while (c < 10)
+	{
+		i = 0;
+		while (i < 15)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+			i++;
+		}
+		c++;
 		_putchar('\n');
+	}
+}
