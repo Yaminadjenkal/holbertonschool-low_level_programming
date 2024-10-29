fb5ffa28e2db94f76764ace2806f7dc957c3ae0d
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-29 13:10:10 +0000

    test

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -4,21 +4,26 @@
  *@n:character to print
  *description:new ligne if n is 0
  */
-void print_diagonal(int n) {
-    int c, i;
+void print_diagonal(int n)
+	{
+	int c, i;
     c = 0;
 
-    if (n <= 0) {
-        _putchar('\n');
-    } else {
-        while (c < n) {
-            for (i = 0; i < c; i++) {
-                _putchar(' ');
-            }
-            _putchar('\\');
-            _putchar('\n');
-            c++;
-        }
+    if (n <= 0) 
+	{
+	 _putchar('\n');
     }
+	else
+	{
+while (c < n) 
+{
+for (i = 0; i < c; i++) 
+{
+_putchar(' ');
 }
-
+_putchar('\\');
+_putchar('\n');
+c++;
+}
+}
+}
