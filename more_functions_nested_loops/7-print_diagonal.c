900688630431204bbbd8c144dd0706d6fdce2d95
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-29 09:54:10 +0000

    test

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_diagonal - print a diagonal
+ *@n:character to print
+ *description:new ligne if n is 0
+ */
+void print_diagonal(int n)
+{
+	int c; 
+
+	c = 0
+	while (c < n)
+{
+	_putchar(\);
+			c++
+			}
+			{
+			_putchar('\n');
+			}
+			}
