83849b660ed6436445298062f0bde148b2748725
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-29 14:34:15 +0000

    test

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -5,20 +5,24 @@
  * description :use only _putchar to print
  */
 void print_square(int size)
+	int c, i;
+	c = 0;
+
+	if (n <= 0) 
 {
-	int i, j;
-	if (size <= 0)
-	{
 		_putchar('\n');
 	}
-	else 
-	{
-	for (i= 0; i < size; i++)
-		for (j = 0; j < size; j++)
-	
-	{
-		_putchar('#');
+else
+{
+		while (c < size) 
+		{
+			for (i = 0; i < c; i++)
+			{
+				
+				_putchar('#');
+			}
+			_putchar('\n');
+			c++;
+		}
 	}
-	_putchar('\n');
-}
 }
