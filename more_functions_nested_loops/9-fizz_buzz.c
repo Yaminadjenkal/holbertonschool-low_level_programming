6998513f782dcb46d80b7c051f78bc93eb1dd5c3
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-30 09:32:12 +0000

    test

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i ++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0) 
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
@@ -21,19 +21,20 @@ int main(void)
 		{
 			printf("Fizz");
 		}
-	       	else if (i % 5 == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
-	       	else
+			else
 		{
 			printf("%d", i);
 		}
-		if (i < 100) 
+		if (i < 100)
 		{
 			printf(" ");
 		}
 	}
 	printf("\n");
+
 	return 0;
 }
