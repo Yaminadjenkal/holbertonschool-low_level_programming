c35979615d9a207e2c73cd4f3bf1c6d3bd321e66
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-30 09:11:58 +0000

    test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: return 0 if success
+ * printf to print words
+ */
+int main(void)
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0) 
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+	       	else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+	       	else
+		{
+			printf("%d", i);
+		}
+		if (i < 100) 
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return 0;
+}
