63d6c5863035b08ffae272177e29861c4a8e2fd8
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-31 13:31:13 +0000

    test

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ *_strlen - returns the length of a string
+ *@s: pointer to the string
+ * return the value of leng
+ */
+int _strlen(char *s)
+{
+	int leng = 0;
+	for (int leng = 0;leng <= 0; leng++)
+return (0);		
+}
+
+
