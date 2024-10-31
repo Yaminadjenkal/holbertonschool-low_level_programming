06614b1a5403d048fef84cad81a3ee4bcdfb34f6
Yaminadjenkal <mina20101951@hotmail.com>
2024-10-31 11:16:05 +0000

    test

---
+++ 0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+ * reset_to_98 - updates the value
+ * return 0 if success
+ * description : function that takes a pointer to an int as parameter and updates the value
+ */
+void reset_to_98(int *n)
+{
+	int n = 98;
+	int * pa = &a;
+return (0);	
+}
