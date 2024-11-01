a4ee73f999d6a86cae3f2f82ea8f570483e3d296
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-01 10:45:20 +0000

    test

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/*
+ * _puts - prints a string
+ * return nothing
+ * use only the command _puts
+ */
+void _puts(char *str);
+	char *str [] = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
