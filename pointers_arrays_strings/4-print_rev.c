a1b5b17ef939ba4695e29179a2d11f1b6c806294
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-01 11:15:48 +0000

    test

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,12 @@ void print_rev(char *s);
 {
 	int leng = 0;
 		int i;
-	while (s[len] != '\0') 
+
+	while (s[len] != '\0')
 	{
 		leng++;
 	}
-	for (i = leng - 1; i >= 0; i--;)
+	for (i = leng - 1; i >= 0; i--)
 	{
 		_putchar('\n');
 	}
