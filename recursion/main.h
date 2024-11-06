2fbfdd410bc22fcab64bd932d4ee28acb8d4c507
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-06 21:23:32 +0000

    test

--- recursion/main.h
+++ recursion/main.h
@@ -8,4 +8,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int guess);
+int is_prime_helper(int n, int i);
+int is_prime_number(int n);
 #endif /* ¨MAIN_H */
