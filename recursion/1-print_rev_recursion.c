f2ed947a48ad4249b9067b9e7a7b4670a50619e5
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-06 13:55:03 +0000

    test

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -4,10 +4,12 @@
  * _print_rev_recursion - Prints a string in reverse
  * @s: The string to be printed
  */
-void _print_rev_recursion(char *s) {
-    if (*s == '\0') {
-        return;
-    }
-    _print_rev_recursion(s + 1);
-    _putchar(*s);
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
