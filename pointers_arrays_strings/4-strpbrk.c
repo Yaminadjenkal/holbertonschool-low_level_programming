0610f00c739440ff14abb0c44f4f9f5474e72301
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-06 09:14:06 +0000

    test

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -8,17 +8,20 @@
  * Return: Pointer to the byte in s that matches one of the bytes in accept,
  *         or NULL if no such byte is found
  */
-char *_strpbrk(char *s, char *accept) {
-	while (*s) {
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
 		char *a = accept;
-		while (*a) {
-			if (*s == *a) {
-				return s;
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				return (s);
 			}
 			a++;
 		}
 		s++;
 	}
-	return NULL;
+	return (NULL);
 }
-
