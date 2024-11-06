7fc949a7e48c525e31539764d1dae80e4455d841
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-06 09:52:38 +0000

    test

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints the chessboard
+ * @a: Pointer to an array of 8 characters
+ */
+void print_chessboard(char (*a)[8])
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
