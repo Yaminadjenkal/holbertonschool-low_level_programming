228b8e7362b63c0ff03979b6e38d8976467ce445
root <mina20101951@hotmail.com>
2024-11-08 20:37:59 +0000

    test

---
+++ argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings representing the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+    return 0;
