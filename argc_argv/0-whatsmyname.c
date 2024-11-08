1506b7653d520c22c746b7a2de2609e9737811d6
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-08 09:04:41 +0000

    test

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ *  * main - Programme qui affiche son propre nom
+ *  @argc: Nombre d'arguments passés au programme (non utilis�)
+ *  @argv: Tableau de chaînes contenant les arguments.
+ * Return: 0 si le programme se termine avec succès.
+ */
 
 int main(int argc, char **argv)
 {
