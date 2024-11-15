0d65a0b7b10da4a01e50ccf173f5974fea56bb45
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-15 14:40:39 +0000

    test

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * array_range - fonction pour cr√er un tableau
- * @min : valeur minimal incluse dans le tableau
- * @max : valeur maximal incluse dans le tableau
+ * array_range - Create array of integers filled with numbers from min to max
+ * @min: First number in the array
+ * @max: Largest number in the array
  *
- *Return : pointeur vers le tableau nouvellemenit cr√©e ou NUL en cas d'echec
+ * Return: Pointer to new array, NULL if failed
  */
 
 int *array_range(int min, int max)
