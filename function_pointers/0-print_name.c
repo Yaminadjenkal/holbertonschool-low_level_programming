3e2c32b7602d4cdefbc0f51aff0a98da02a51339
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-18 13:58:20 +0000

    test

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name-fonction that print name
+ *@*name the caraters to print 
+ *@f the pointer that point to the function
+ *return : Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+
