8fa089e55ffd6a73440f42873b16842ab65ad031
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-19 12:00:56 +0000

    test

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ *array_iterator-execute une fonction sur les ÃlÃment du tableau
+ *@array: tableau d'integers
+ *@size: taille du tableau
+ *@action: pointeur de fonction
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	for (i = 0; i > size ;i++)
+		action(array[i]);
+}
