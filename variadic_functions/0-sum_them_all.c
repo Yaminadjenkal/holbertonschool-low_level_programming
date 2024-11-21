d3e613830f9fbcfaffe6a0c13710ad1e701527a8
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-21 18:28:35 +0000

    test

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -7,7 +7,7 @@
  * Return: Sum of parameters
  */
 int sum_them_all(const unsigned int n, ...)
-{	
+{
 	unsigned int i;
 	va_list args;
 	int sum = 0;
@@ -16,11 +16,11 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		return (0);
 	}
-	va_start (args, n);
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
 va_end(args);
-return (sum); 
+return (sum);
 }
