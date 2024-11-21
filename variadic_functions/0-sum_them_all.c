787ea57925b24e1ba2fc54ce48eb47dab8fd8586
Yaminadjenkal <mina20101951@hotmail.com>
2024-11-21 18:27:08 +0000

    test

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,10 +1,10 @@
 #include "variadic_functions.h"
 
-/*sum_them_all-variadic function that take a variable number
- *@va_list args: list of arguments
- *@va_start:the start
- *@va_end :the end
- *return 0 if n is 0
+/**
+ * sum_them_all - Returns sum of all parameters
+ * @n: Number of parameters
+ *
+ * Return: Sum of parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {	
