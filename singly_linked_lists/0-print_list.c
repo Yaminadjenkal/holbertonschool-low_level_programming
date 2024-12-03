9b46eb108f8a69a7e33b1f74093bef38a175fb3c
Yaminadjenkal <mina20101951@hotmail.com>
2024-12-03 17:09:43 +0000

    test my code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,18 +1,22 @@
 #include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
 
-size_t print_list(const list_t *h) {
-    size_t count = 0;
-    const list_t *current = h;
-
-    while (current != NULL) {
-        if (current->str == NULL) {
-            printf("[0] (nil)\n");
-        } else {
-            printf("[%lu] %s\n", (unsigned long)strlen(current->str), current->str);
-        }
-        count++;
-        current = current->next;
-    }
-    return count;
+size_t print_list(const list_t *h)
+{
+	size_t life;
+		life = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		life++;
+	}
+	return (life);
 }
-
