f9512a7066c25fe6ec23924e8cea23c21ab0f025
Yaminadjenkal <mina20101951@hotmail.com>
2024-12-04 18:43:08 +0000

    fix my code

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - Ajoute un nouveau nœud au début d'une liste dlistint_t.
+ * @head: Double pointeur vers la tête de la liste.
+ * @n: Entier à insérer dans le nouveau nœud.
+ * Return: L'adresse du nouvel élément, ou NULL en cas d'échec.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+
+	return (new_node);
+}
