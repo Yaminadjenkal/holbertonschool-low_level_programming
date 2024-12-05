e6d1646824ddfac7b6dd3279ef484f0fb534fee7
Yaminadjenkal <mina20101951@hotmail.com>
2024-12-05 10:42:18 +0000

    my code"

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,32 +2,35 @@
 #include <stdlib.h>
 
 /**
- *add_dnodeint_end - add a new node at list of dlistint_t
- *@head : double pointer to the head of the list
- *@n : integer to insert in the new node
- * Return : the new node or NULL if it fails
+ * add_dnodeint_end - Ajoute un nouveau nœud à la fin d'une liste dlistint_t.
+ * @head: Double pointeur vers la tête de la liste.
+ * @n: Entier à insérer dans le nouveau nœud.
+ * Return: L'adresse du nouvel élément, ou NULL en cas d'échec.
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node; *temp;
-	new_node = malloc (sizeof (dlistint_t);
-			if (new_node == NULL)
-			return (NULL);
-			new_node->n = n; 
-			new_node->next = NULL;
+    dlistint_t *new_node, *temp;
 
-			if (*head == NULL)
-			{
-			new_node->prev = NULL;
-			*head = new_node;
-			return (new_node);
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
 
-			}
-			temps = *head;
+    new_node->n = n;
+    new_node->next = NULL;
 
-			while (temp->next != NULL)
-			temp = temp->next;
-			temps->next = new_node;
-			new_node->prev = temp;
-			return (new_node)
-			}
+    if (*head == NULL)
+    {
+        new_node->prev = NULL;
+        *head = new_node;
+        return (new_node);
+    }
+
+    temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = new_node;
+    new_node->prev = temp;
+
+    return (new_node);
+}
