77db18f3b8e9da409ed5d661d53c3afe6937f2ae
Yaminadjenkal <mina20101951@hotmail.com>
2024-12-06 20:24:23 +0000

    fix my code

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - Libère une liste doublement chaînée.
+ * @head: Pointeur vers la tête de la liste.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
